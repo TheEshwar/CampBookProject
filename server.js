0d1b0413acaf68a5229fa5fb2c5c85c1d4ae17c3
TheEshwar <deshmukh.eshwar@gmail.com>
2023-07-18 14:47:02 +0530

    Added routes for bootcamps

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express")
-const dotenv = require("dotenv")
-
-dotenv.config({path: './config/config.env'})
-
 const app = express()
+const dotenv = require("dotenv")
+dotenv.config({ path: './config/config.env' })
+
 const PORT = process.env.PORT || 5000
+
+const bootcamps = require('./routes/bootcamps')
+app.use('/api/v1/bootcamps', bootcamps)
 
 app.listen(PORT, console.log(`\nServer is running in '${process.env.NODE_ENV}' on PORT - ${PORT}`));
