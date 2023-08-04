32e4db84ab553c0be07008f4a9ec773cb3b88c50
TheEshwar <deshmukh.eshwar@gmail.com>
2023-08-04 18:53:23 +0530

    Added

--- models/Course.js
+++ models/Course.js
@@ -1,40 +1,69 @@
 const mongoose = require('mongoose')
 const CourseSchema = new mongoose.Schema({
-    title:{
+    title: {
         type: String,
         trim: true,
         require: [true, 'Please add a course title']
     },
-    description:{
+    description: {
         type: String,
         require: [true, 'Please add a course description']
     },
-    weeks:{
+    weeks: {
         type: String,
         require: [true, 'Please add number of weeks']
     },
-    tuition:{
+    tuition: {
         type: Number,
         require: [true, 'Please add a tuition cost']
     },
-    minimumSkill:{
+    minimumSkill: {
         type: String,
         require: [true, 'Please add a minimum skill'],
         enum: ['beginner', 'intermediate', 'advanced']
     },
-    scholarship:{
+    scholarship: {
         type: Boolean,
         default: false,
     },
-    createdAt:{
+    createdAt: {
         type: Date,
         require: Date.now
     },
-    bootcamp:{
+    bootcamp: {
         type: mongoose.Schema.ObjectId,
         ref: 'Bootcamp',
         required: true
-    },    
+    },
+})
+
+// Static method to get average course cost
+CourseSchema.statics.getAverageCost = async function (bootcampId) {
+    console.log('Calculating course average cost'.blue);
+
+    const obj = await this.aggregate([
+        {
+            $match: {bootcamp: bootcampId}
+        },
+        {
+            $group: {
+                _id: '$bootcamp',
+                averageCost: {$avg: '$tuition'}
+            }
+        }
+    ])
+
+    console.log("\n",obj, "\n");
+}
+
+// Call getAverageCost after save
+CourseSchema.post('save', function () {
+    this.constructor.getAverageCost(this.bootcamp)
+})
+
+// Call getAverageCost before save
+CourseSchema.pre('remove', function () {
+    this.constructor.getAverageCost(this.bootcamp)
 })
 
 module.exports = mongoose.model('Course', CourseSchema);
--- seeder.js
+++ seeder.js
@@ -17,7 +17,7 @@ const courses = JSON.parse(fs.readFileSync(`${__dirname}/_data/courses.json`, 'u
 const importData = async () => {
     try {
         await Bootcamp.create(bootcamps)
-        await Course.create(courses)
+        // await Course.create(courses)
         console.log('Data imported...'.green.inverse);
         process.exit()
     }
@@ -29,7 +29,7 @@ const importData = async () => {
 const deleteData = async () => {
     try {
         await Bootcamp.deleteMany()
-        await Course.deleteMany()
+        // await Course.deleteMany()
         console.log('Data destroyed...'.red.inverse);
         process.exit()
     }
