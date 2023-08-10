5ab4e7bae1e5013e353f8a0937c90a00781bbf97
TheEshwar <deshmukh.eshwar@gmail.com>
2023-08-10 18:43:51 +0530

    Added forgot password - generate Token

--- controllers/auth.js
+++ controllers/auth.js
@@ -75,3 +75,25 @@ exports.getMe = asyncHandler(async (req, res, next) => {
         data: user
     })
 })
+
+// @desc    Forgot password
+// @routes  POST /api/v1/auth/forgotPassword
+// @access  Public
+exports.forgotPassword = asyncHandler(async (req, res, next) => {
+    const user = await User.findOne({email: req.body.email})
+
+    if(!user){
+        return next(new ErrorResponse('There is no user with that email', 404))
+    }
+
+    // Get reset token
+    const resetToken = user.getResetPasswordToken()
+
+    await user.save({validateBeforeSave: false})
+
+    console.log('\n reset token :- ', resetToken);
+    res.status(200).json({
+        success: true,
+        data: user
+    })
+})
